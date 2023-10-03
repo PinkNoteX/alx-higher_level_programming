00eb1ee7b7f64fadd3cf98d7959e45b5d0e1d2b3
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-03 11:13:13 -0700

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 x = 0
 while x < 100:
     if x == 99:
-        print("{}".format(x),end="")
+        print("{}".format(x),end="\n")
     else:
         print("{:02}".format(x),end=", ")
     x = x + 1
