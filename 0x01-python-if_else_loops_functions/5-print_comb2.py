146603bc67875c0180886b4b51c06164942d5c7f
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-03 11:11:12 -0700

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 x = 0
-y = 0
-while x < 10:
-    while y < 10:
-        if x == 9 and y == 9:
-            print("{}{}".format(x,y),end="")
-        else:
-            print("{}{}".format(x,y),end=", ")
-        y = y + 1
+while x < 100:
+    if x == 99:
+        print("{}".format(x),end="")
+    else:
+        print("{}".format(x),end=", ")
     x = x + 1
-    y = 0
