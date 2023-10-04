720d6fb381c503eeb3c18968911553937efc9111
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-03 17:31:41 -0700

    ADV 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,8 @@
 r = 25
 rev = 0
 while r >= 0 and r < 26:
-     rev = r + ord('A')
-     if r % 2 == 1:
-         rev = rev + 32
-     print("{}".format(chr(rev)), end='')
-     r = r - 1
+    rev = r + ord('A')
+    if r % 2 == 1:
+        rev = rev + 32
+    print("{}".format(chr(rev)), end='')
+    r = r - 1
