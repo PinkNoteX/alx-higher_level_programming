a1efe674158de9fb4fdaecf23cfdf88492708fec
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-09 03:27:33 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    noC = for x in my_string if x not in 'Cc'
-    return (''.join(noC))
+    noC = x for x in my_string if x not in 'Cc'
+    return("".join(noC))
