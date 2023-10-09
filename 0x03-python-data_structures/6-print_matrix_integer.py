4d6a965817440e71272bda3a53cd6b7ca7fa278b
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-09 03:49:36 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for el in matrix:
         for r in el:
-            print("{:d}".format(r), end=" ")
-            if r == el[len(el) - 1]:
-                print("", end="")
+            print("{:d}".format(r), end="")
+            if r != el[len(el) - 1]:
+                print(" ", end="")
         print("")
