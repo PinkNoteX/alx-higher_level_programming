ce5ba9c470d2238662d5011f5992fae1c63be953
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-09 05:43:44 -0700

    ADV Prep

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_python_list_info - print list info
+ * @p: don't know yet
+ *
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+
+//researching.........
+}
