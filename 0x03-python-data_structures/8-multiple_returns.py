a09cb6a71a4dead96fcd30e92003c2ad7cc99126
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-09 04:21:53 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if length == 0:
-        return 0, 0
+        return 0, None
     else:
         return length, first
