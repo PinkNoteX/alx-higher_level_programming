3ce3284d120e6d330034c0fb4990af51a2cb3c88
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 13:16:08 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    diclist = list(dic)
     store = 0
     if roman_string is None or isinstance(roman_string, str) is False:
         return 0
-    for r in roman_string:
-        for x in diclist:
-            if x == r:
-                store = store + dic.get(x)
+    for r in range(len(roman_string)):
+        if dic.get(roman_string[r], 0) == 0:
+            return 0
+        if r != (len(roman_string) - 1) and\
+                dic[roman_string[r]] < dic[roman_string[r + 1]]:
+            store = store + dic[roman_string[r]] * -1
+        else:
+            store = store + dic[roman_string[r]]
     return store
