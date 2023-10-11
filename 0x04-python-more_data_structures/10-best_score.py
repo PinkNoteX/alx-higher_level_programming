21c590d7dbcd5ad47a28ef71ae6fde66045fa848
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 09:52:38 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,12 +3,13 @@ def best_score(a_dictionary):
     if a_dictionary is not None:
         newlist = list(a_dictionary)
         listlen = len(newlist)
-        x = store = value = 0
+        x = store = value = xst = 0
         while x < listlen:
             value = a_dictionary.get(newlist[x])
             if value > store:
                 store = value
+                xst = x
             x = x + 1
-        return store
+        return newlist[xst]
     else:
         return None
