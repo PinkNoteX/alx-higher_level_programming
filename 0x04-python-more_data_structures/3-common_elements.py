a3505aa5a6721935b953ed1f2b3bc269b3802dfd
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 07:42:38 -0700

    Tasl 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     newlist = []
-    flen = len(set_1)
-    slen = len(set_2)
-    x = r = 0
-    while r < flen:
-        while x < slen:
-            if set_1[r] == set_2[x]:
-                newlist.append(set_1[r])
-            x = x + 1
-        r = r + 1
+    for l in set_1:
+        if l in set_2:
+            newlist.append(l)
+
     return (newlist)
