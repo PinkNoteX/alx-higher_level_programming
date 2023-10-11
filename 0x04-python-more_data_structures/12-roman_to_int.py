170666a3abd3d2505397f92b173e0e7ab987202d
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 12:26:36 -0700

    Tas k12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     diclist = list(dic)
     store = 0
     if roman_string and isinstance(roman_string, str):
