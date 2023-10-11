a72f6c5767941e5c191c9f171acdc270583d99bd
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 07:51:24 -0700

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = set_1 + set_2
-    newlist = list(new_list)
+    new_set = set_1
+    new_set.symmetric_difference_update(set_2)
+    newlist = list(new_set)
     return (newlist)
