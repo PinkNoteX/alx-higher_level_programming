e43402bd4b6e9702e400ae77adcd7fcd4900226f
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 07:46:28 -0700

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,8 @@
 def common_elements(set_1, set_2):
     newlist = []
 
-    for l in set_1:
-        if l in set_2:
-            newlist.append(l)
+    for la in set_1:
+        if la in set_2:
+            newlist.append(la)
+
     return newlist
