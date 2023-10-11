a84c92efc9dca7b71740970598c72e19fa2e4b54
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 13:42:31 -0700

    ADV 4 prep

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <Python.h>
+/*
+ * yes description here....
+ */
+void print_python_list(PyObject *p)
+{
+
+}
+/*
+ * imagine description here
+ */
+void print_python_bytes(PyObject *p)
+{
+
+}
