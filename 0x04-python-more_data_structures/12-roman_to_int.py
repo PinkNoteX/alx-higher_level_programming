2a44cd36ee9de689048bd013fcd1f3168f87527e
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-11 12:21:07 -0700

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dic = {'I': 1, 'V': 5,
+            'X': 10, 'L': 50,
+            'C': 100, 'D': 500,
+                        'M': 1000}
+    diclist = list(dic)
+    store = 0
+    for r in roman_string:
+        for x in diclist:
+            if x == r:
+                store = store + dic.get(x)
+    return store
