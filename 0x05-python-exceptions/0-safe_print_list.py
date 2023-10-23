4efdc2ee32ff7800ed23e8fc41e82ab8df67f7b3
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-23 12:26:07 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,6 @@ def safe_print_list(my_list=[], x=0):
                 print("{}".format(my_list[m]), end='')
                 counter += 1
             except IndexError:
-                print("")
-                return(counter)
+                break
     print("")
     return(counter)
