d031fcd9245649856790848d682c281f7f097ded
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-24 15:31:48 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class defines square"""
+
+
+class Square:
+    """class defines square"""
+    def __init__(self, size):
+        """Initializes private attribute size """
+        self.__size = size
