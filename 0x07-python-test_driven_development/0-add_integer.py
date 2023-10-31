9bdbdbd5fe681db7f1a42d0e73889aa833c0adb4
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-30 20:33:40 -0700

    Task 0 done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""add integars"""
+
+
+def add_integer(a, b=98):
+    """ add two integars"""
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+            raise TypeError('b must be an integer')
+    return int(a) + int(b)
