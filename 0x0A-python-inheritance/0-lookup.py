1f046314d9bc5c0379cdba27612fd0ffc4401c8d
PinkNoteX <omarfarghaly8@gmail.com>
2023-11-06 03:35:02 -0800

    Task 1 and pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """lookup function"""
+
+
 def lookup(obj):
     """the function"""
     return ([r for r in dir(obj)])
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""mylist"""
+
+
+def MyList(list):
+    """print sorted list"""
+    def print_sorted(self):
+        print(sorted(self))
