d848941ea45fd07dd2b079477491256ec206f8d1
PinkNoteX <omarfarghaly8@gmail.com>
2023-11-13 10:52:37 -0800

    Adv test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ first class """
 import json
+import csv
+import os.path
 
 
 class Base:
@@ -68,3 +70,51 @@ class Base:
         for dictionary in objlist:
             alist.append(cls.create(**dictionary))
         return alist
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ load from csv """
+        if os.path.exists("{}.csv".format(cls.__name__)) is False:
+            return []
+
+        with open("{}.csv".format(cls.__name__), 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            alist = list(reader)
+
+        if cls.__name__ == "Rectangle":
+            akeys = ['id', 'width', 'height', 'x', 'y']
+        else:
+            akeys = ['id', 'size', 'x', 'y']
+        retlist = []
+        for x in alist:
+            dic_csv = {}
+            for r in enumerate(x):
+                dic_csv[akeys[r[0]]] = int(r[1])
+            retlist.append(dic_csv)
+        inlist = []
+        for m in range(len(retlist)):
+            inlist.append(cls.create(**alist[m]))
+        return inlist
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ save to csv """
+        altd = []
+        if cls.__name__ == "Rectangle":
+            akeys = ['id', 'width', 'height', 'x', 'y']
+            adic = [0, 0, 0, 0, 0]
+        else:
+            adic = [0, 0, 0, 0]
+            akeys = ['id', 'size', 'x', 'y']
+        if not list_objs:
+            pass
+        else:
+            for m in list_objs:
+                for x in range(len(akeys)):
+                    adic[x] = m.to_dictionary()[akeys[x]]
+                altd.append(adic[:])
+        with open('{}'.format(cls.__name__), 'w', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(altd)
+
+
