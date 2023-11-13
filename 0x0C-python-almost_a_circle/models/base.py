dc707cf2cc40e70584600498d590b34abee3b73c
PinkNoteX <omarfarghaly8@gmail.com>
2023-11-13 09:33:25 -0800

    Task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,17 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ create instance """
+        if cls.__name__ == 'Rectangle':
+            new = cls(1, 1)
+            new.update(**dictionary)
+            return new
+        elif cls.__name__ == 'Square':
+            new = cls(1)
+            new.update(**dictionary)
+            return new
+        else:
+            return None
