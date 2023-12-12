3053d99843cee3f0962c43ba5786833d6421e20a
PinkNoteX <omarfarghaly8@gmail.com>
2023-12-12 14:45:49 -0800

    Task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- create a table second_table in the database hbtn_0c_0 in MySQL server and add multiples rows.
+INSERT INTO second_table
+       (id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
