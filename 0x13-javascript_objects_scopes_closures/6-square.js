b2b068de1d00e8240666962f99f50058abaf7bc1
PinkNoteX <omarfarghaly8@gmail.com>
2024-01-09 16:18:47 -0800

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undifined) {
+      c = 'X';
+    }
+    for (let id = 0; id < this.height; id++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
