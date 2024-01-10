3f35ab9a8b1bb61c5f1e5816f22e0f5c048f0700
PinkNoteX <omarfarghaly8@gmail.com>
2024-01-09 16:31:31 -0800

    Task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const rev = [];
+  let len = list.length - 1;
+  while (0 <= len) {
+    rev.push(list[len]);
+    len--;
+  }
+  return rev;
+};
