e946141e2961c3f4261f54a2c1e3d9660926711c
PinkNoteX <omarfarghaly8@gmail.com>
2024-01-26 17:19:58 -0800

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# post email
+from urllib import request
+from urllib import parse
+import sys
+
+value = {'email': sys.argv[2]}
+data = parse.urlencode(value).encode('ascii')
+handler = request.Request(sys.argv[1], data)
+with request.urlopen(handler) as res:
+    page = res.read().decode('utf-8')
+    print(page)
