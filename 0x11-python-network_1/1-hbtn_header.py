c11786fb6e5f3117632868f69299cbd9f6472f6e
PinkNoteX <omarfarghaly8@gmail.com>
2024-01-26 17:26:48 -0800

    Task 1 test

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# display the value of the x-request-id var in header of the response
-from urllib import request
+# display the value of the x-request-id var in header
+import urllib.request
 import sys
 
-with request.urlopen(sys.argv[1]) as res:
+with urllib.request.urlopen(sys.argv[1]) as res:
     print(res.getheader('X-Request-Id'))
